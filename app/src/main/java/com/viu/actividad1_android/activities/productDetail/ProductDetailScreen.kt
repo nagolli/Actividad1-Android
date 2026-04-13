061062728a09f7a33e7d5070e9b1943ee0cc0d77
ursgarsan <ugsugs333@gmail.com>
2026-04-13 20:09:38 +0200

    REFACTOR: import sin usar eliminado

--- app/src/main/java/com/viu/actividad1_android/activities/productDetail/ProductDetailScreen.kt
+++ app/src/main/java/com/viu/actividad1_android/activities/productDetail/ProductDetailScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
